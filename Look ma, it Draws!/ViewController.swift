63993be4d29383b58bd383fcba15da1039cb6806
student <s528760@ch140027m.nwmissouri.edu>
2017-11-27 17:42:19 -0600

    Initial Commit

---
+++ Look ma, it Draws!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Look ma, it Draws!
+//
+//  Created by Reddygari,Amarendar Reddy on 11/27/17.
+//  Copyright © 2017 Reddygari,Amarendar Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
